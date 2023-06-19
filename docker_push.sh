426570dc7b136879245a43b131ac771e7dc77a1a
Sumit Chohan <Sumit.Chohan@ttx.com>
2023-06-19 16:12:39 -0500

    wip

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,11 @@
  
 docker build . -t localhost/node-api:latest -f dockerfile
 docker run -d -p 8080:8080 localhost/node-api:latest 
-docker login  
+docker login -u chauhansumit
+#enter PAT 
 docker image tag localhost/node-api:latest docker.io/chauhansumit/node-api:latest
 docker push docker.io/chauhansumit/node-api:latest
+
+
+docker run -d -p 8080:8080 docker.io/chauhansumit/node-api:latest
 
